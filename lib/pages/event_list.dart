2176c50ddaf3cb1793aef69776297a97f2eeef2d
Peter Vida <petike997@gmail.com>
2021-11-21 23:27:32 +0100

    get started with facebook api

--- lib/pages/event_list.dart
+++ lib/pages/event_list.dart
@@ -2,13 +2,29 @@ import 'dart:developer';
 
 import 'package:cuddly_carnival/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:i18n_extension/default.i18n.dart';
+import 'package:http/http.dart' as http;
 
 import '../widgets/event_entry.dart';
 import '../routes.dart';
 
+// const String eventsPath = 'http://echo.jsontest.com/key/';
+const String eventsPath =
+    'https://graph.facebook.com/v11.0/me/events?fields=id,name&access_token=';
+// const String eventsPath = 'http://headers.jsontest.com/';
+
 class EventList extends StatefulWidget {
-  EventList({Key? key}) : super(key: key);
+  const EventList({required this.accessToken, Key? key}) : super(key: key);
+
+  Future<dynamic> getEvents() async {
+    http.Response response =
+        await http.get(Uri.parse(eventsPath + accessToken.token));
+    log(response.statusCode.toString());
+    return response.body;
+  }
+
+  final AccessToken accessToken;
 
   @override
   State<StatefulWidget> createState() => _EventListState();
@@ -17,6 +33,7 @@ class EventList extends StatefulWidget {
 class _EventListState extends State<EventList> {
   @override
   Widget build(BuildContext context) {
+    log(widget.accessToken.token);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Events'.i18n),
@@ -27,14 +44,28 @@ class _EventListState extends State<EventList> {
               icon: const Icon(Icons.search)),
         ],
       ),
-      body: ListView(
-        children: const <Widget>[
-          EventEntry(),
-        ],
+      body: FutureBuilder(
+        future: widget.getEvents(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          log(snapshot.toString());
+          return ListView(
+            children: [
+              Text(snapshot.connectionState.toString()),
+              Text(snapshot.toString()),
+              Text(snapshot.data.toString()),
+            ],
+          );
+        },
       ),
+      //  ListView(
+      //   children: const <Widget>[
+      //     EventEntry(),
+      //   ],
+      // ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () => {Navigator.pushNamed(context, ROUTE.Discover)},
-          child: const Icon(Icons.add)),
+        onPressed: () => {Navigator.pushNamed(context, ROUTE.Discover)},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
